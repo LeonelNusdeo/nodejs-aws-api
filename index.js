7dc10cecf0d5765bcddf2f90650a43bfd9449e04
Leonel Nusdeo <leonel.nusdeo@gmail.com>
2024-07-02 22:57:09 -0300

    Created basic server with Express. Using express-fileupload middleware.

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import fileUpload from 'express-fileupload';
+
+const app = express();
+
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: './uploads'
+}));
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the server with S3'})
+})
+
+app.post('/files', (req, res) => {
+    console.log(req.files);
+    res.json({ message: 'File uploaded'})
+})
+
+
+app.listen(3000);
+console.log(`Server on port ${3000}`);
