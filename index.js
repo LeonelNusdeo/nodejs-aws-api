f8733f591dda48edc62da763ba9025d8cb94cb0c
Leonel Nusdeo <leonel.nusdeo@gmail.com>
2024-07-18 22:39:47 -0300

    Created uploadFile() to S3 bucket

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-import express from 'express';
-import fileUpload from 'express-fileupload';
-
-import './config/aws.js';
+import express from "express";
+import fileUpload from "express-fileupload";
+import { uploadFile } from "./s3.js";
 
 const app = express();
 
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: "./uploads",
+    })
+);
 
-app.use(fileUpload({
-    useTempFiles: true,
-    tempFileDir: './uploads'
-}));
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the server with S3" });
+});
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the server with S3'})
-})
-
-app.post('/files', (req, res) => {
-    console.log(req.files);
-    res.json({ message: 'File uploaded'})
-})
-
+app.post("/files", async (req, res) => {
+    const result = await uploadFile(req.files.file);
+    res.json({ result });
+});
 
 app.listen(3000);
 console.log(`Server on port ${3000}`);
